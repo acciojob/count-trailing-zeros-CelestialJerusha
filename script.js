a81e9b6fef0656d8c7dd237bfb5e7145ace6eead
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-28 20:53:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let a = n;
+                    for(let i=1;i<n;i++){
+                        a=a*i;
+                    }
+                    let count=0;
+                    while(a!==0){
+                        let rem = a%10;
+                        if(rem === 0){
+                            count++;
+                        }
+                        a=a/10;
+                    }
+                    return count;
 }
 
 const input = prompt("Enter a number");
